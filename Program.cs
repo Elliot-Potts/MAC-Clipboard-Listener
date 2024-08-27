bec2610355d04efe3f40402874af7ac3ad1a6558
Elliot Potts <elliot.potts@northumbria.ac.uk>
2024-08-27 15:40:44 +0100

    Added basic MAC address replacement
    MAC address format option selection; MAC address clipboard overwriting.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace MACAddressMonitor
 {
@@ -21,6 +22,15 @@ namespace MACAddressMonitor
         private NotifyIcon trayIcon;
         private ContextMenuStrip trayMenu;
         private ClipboardMonitor clipboardMonitor;
+        private MacFormat selectedFormat = MacFormat.CiscoNotation;
+        private bool ignoringNextClipboardUpdate = false;
+
+        private enum MacFormat
+        {
+            CiscoNotation,
+            ColonSeparated,
+            HyphenSeparated
+        }
 
         public MacAddressMonitorContext()
         {
@@ -32,6 +42,13 @@ namespace MACAddressMonitor
         private void InitializeComponent()
         {
             trayMenu = new ContextMenuStrip();
+
+            ToolStripMenuItem formatMenu = new ToolStripMenuItem("Select MAC Format");
+            formatMenu.DropDownItems.Add("Cisco Notation", null, (s, e) => SetMacFormat(MacFormat.CiscoNotation));
+            formatMenu.DropDownItems.Add("Colon Separated", null, (s, e) => SetMacFormat(MacFormat.ColonSeparated));
+            formatMenu.DropDownItems.Add("Hyphen Separated", null, (s, e) => SetMacFormat(MacFormat.HyphenSeparated));
+
+            trayMenu.Items.Add(formatMenu);
             trayMenu.Items.Add("Show Notification", null, OnShowNotification);
             trayMenu.Items.Add("-"); // Separator
             trayMenu.Items.Add("Exit", null, OnExit);
@@ -43,6 +60,12 @@ namespace MACAddressMonitor
                 Text = "MAC Address Monitor",
                 Visible = true
             };
+        }
+
+        private void SetMacFormat(MacFormat format)
+        {
+            selectedFormat = format;
+            ShowNotification("MAC Format Updated", $"Selected format: {format}");
         }
 
         protected override void Dispose(bool disposing)
@@ -62,31 +85,104 @@ namespace MACAddressMonitor
 
         private void OnShowNotification(object sender, EventArgs e)
         {
-            trayIcon.ShowBalloonTip(3000, "MAC Address Monitor", "MAC address found in clipboard.\nVendor: Example-HP", ToolTipIcon.Info);
+            ShowNotification("MAC Address Monitor", "This is a test notification.");
         }
 
-        private void OnClipboardUpdated()
+        private async void OnClipboardUpdated()
         {
-            string clipboardText = GetClipboardText();
-            if (IsMACAddress(clipboardText))
+            if (ignoringNextClipboardUpdate)
             {
-                trayIcon.ShowBalloonTip(3000, "MAC Address Detected", clipboardText, ToolTipIcon.Info);
+                ignoringNextClipboardUpdate = false;
+                return;
+            }
+
+            try
+            {
+                string clipboardText = await Task.Run(() => GetClipboardText());
+                if (IsMACAddress(clipboardText))
+                {
+                    string formattedMac = ConvertMacFormat(clipboardText, selectedFormat);
+                    if (formattedMac != clipboardText)
+                    {
+                        ignoringNextClipboardUpdate = true;
+                        await Task.Run(() => SetClipboardText(formattedMac));
+                        ShowNotification("MAC Address Formatted", formattedMac);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowNotification("Error", $"An error occurred: {ex.Message}");
             }
         }
 
         private string GetClipboardText()
         {
-            if (Clipboard.ContainsText())
+            string text = string.Empty;
+            var thread = new System.Threading.Thread(() =>
             {
-                return Clipboard.GetText();
-            }
-            return string.Empty;
+                try
+                {
+                    if (Clipboard.ContainsText())
+                    {
+                        text = Clipboard.GetText();
+                    }
+                }
+                catch (Exception)
+                {
+                    // Clipboard was probably in use, we'll just return empty string
+                }
+            });
+            thread.SetApartmentState(System.Threading.ApartmentState.STA);
+            thread.Start();
+            thread.Join();
+            return text;
+        }
+
+        private void SetClipboardText(string text)
+        {
+            var thread = new System.Threading.Thread(() =>
+            {
+                try
+                {
+                    Clipboard.SetText(text);
+                }
+                catch (Exception)
+                {
+                    // Clipboard was probably in use, we'll just skip setting it
+                }
+            });
+            thread.SetApartmentState(System.Threading.ApartmentState.STA);
+            thread.Start();
+            thread.Join();
+        }
+
+        private void ShowNotification(string title, string message)
+        {
+            trayIcon.ShowBalloonTip(3000, title, message, ToolTipIcon.Info);
         }
 
         private bool IsMACAddress(string text)
         {
-            string pattern = @"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$";
+            string pattern = @"^(?i)(([0-9A-F]{2}[:-]){5}([0-9A-F]{2})|([0-9A-F]{4}\.){2}([0-9A-F]{4}))$";
             return Regex.IsMatch(text, pattern);
+        }
+
+        private string ConvertMacFormat(string mac, MacFormat format)
+        {
+            string cleanMac = Regex.Replace(mac.ToUpper(), "[.:-]", "");
+
+            switch (format)
+            {
+                case MacFormat.CiscoNotation:
+                    return Regex.Replace(cleanMac, "(.{4})(.{4})(.{4})", "$1.$2.$3");
+                case MacFormat.ColonSeparated:
+                    return Regex.Replace(cleanMac, "(.{2})(.{2})(.{2})(.{2})(.{2})(.{2})", "$1:$2:$3:$4:$5:$6");
+                case MacFormat.HyphenSeparated:
+                    return Regex.Replace(cleanMac, "(.{2})(.{2})(.{2})(.{2})(.{2})(.{2})", "$1-$2-$3-$4-$5-$6");
+                default:
+                    return mac;
+            }
         }
     }
 
